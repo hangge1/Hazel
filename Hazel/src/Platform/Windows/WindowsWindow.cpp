ecae22a3487037281fd6903aaacb548841e84df9
hangdada <824362330@qq.com>
2024-07-05 12:04:41 +0800

    [feature] 1、添加GraphicsContex接口，实现OpenGL上下文

--- Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -5,8 +5,8 @@
 #include "Hazel/Events/MouseEvent.h"
 #include "Hazel/Events/KeyEvent.h"
 
-#include "glad/glad.h"
 #include <GLFW/glfw3.h>
+#include "Platform/OpenGL/OpenGLContext.h"
 
 namespace Hazel {
 
@@ -45,13 +45,12 @@ namespace Hazel {
 		}
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
-		glfwMakeContextCurrent(m_Window);
-		
+
+		m_Context = new OpenGLContext(m_Window);
+		m_Context->Init();
+
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
-		//GLAD Init
-		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-		HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
 		//Set GLFW callback
 		glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height)
 		{
